54e43cbb6935aaa1e1c153b2a4aaf39bf2e784ba
josevergelin <75032564+josevergelin@users.noreply.github.com>
2021-02-09 18:59:08 -0500

    Ok

---
+++ src/app/my-grid/my-grid.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-grid',
+  templateUrl: './my-grid.component.html',
+  styleUrls: ['./my-grid.component.css']
+})
+export class MyGridComponent {}
