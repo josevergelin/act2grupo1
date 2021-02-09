eb9992c8a326a20a491ee3c5ac98610b024a924b
SUPERTRONICA <eduxworks@gmail.com>
2021-02-08 23:14:17 -0500

    Instalacion de angular instalacion de material creacion de un nav-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,15 @@ import { MyNavComponent } from './my-nav/my-nav.component';
 import {TarjetasComponent} from './tarjetas/tarjetas.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu';
-import {MenuComponent} from './menu/menu.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MyNavComponent,
-    TarjetasComponent,
-    MenuComponent
+    TarjetasComponent
+
 
   ],
   imports: [
